4eb36d11dd31277d5ec47ef488662cd71964c608
Afshin <fallahi_a@yahoo.com>
2023-08-25 10:56:00 -0400

    added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,19 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './entities/user';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type:'postgres',
-      host:'localhost',
-      port: 5432,
-      username: 'postgres',
-      password:'secret',
-      database:'mydatabase',
+      host:process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities:[User]
     }),
     UserModule
